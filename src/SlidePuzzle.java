64feebb10df6055e887d7536a91f576e91821026
Obijuankanobe <joshrocha422@gmail.com>
2017-12-11 21:00:02 -0700

    This is my second commit with the intial class name and a brief description of what the program will do.

---
+++ src/SlidePuzzle.java
@@ -0,0 +1,10 @@
+/**
+ * This is a program that is a clone of the slide puzzel game when complete the user will be able to put the numbers in
+ * order from 1 to 8.
+ * @author  Joshua Rocha
+ */
+public class SlidePuzzle {
+    public static void main (String[] args){
+
+    }
+}
